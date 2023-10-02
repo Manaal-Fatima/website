53d4168aa303aa3a047cf83f25ba2c2503253223
Manaal-Fatima <manaal2fatima@gmail.com>
2023-10-02 20:34:04 +0500

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+return (
+        <div className="cv">
+          <header>
+            <h1>Manaal Fatima</h1>
+            <img src="img.jpg" alt="Your Photo" />
+          </header>
+          <section className="education">
+            <h2>Personal Information</h2>
+            <p>Father Name:M.Shahzad</p>
+            <p>DOB: 13/12/2003</p>
+            <p>Wamt to become a" DEVELOPER"</p>
+          </section>
+          <section className="education">
+            <h2>Education</h2>
+            <p>Studying in 6th semester of BSIT from Punjab University</p>
+            <p>Matric in 1st division from PUNJAB BOARD LAHORE</p>
+            <p>Intermediate from GOVT.degree college Gulshan RaVI</p>
+          </section>
+          
+          <section className="contact">
+            <h2>Contact Information</h2>
+            <p>Email: manaal2fatima@gmail.com</p>
+            <p>Phone: 0321.....</p>
+          </section>
+          
+        </div>
+      );
+    }
+    
 
 export default App;
